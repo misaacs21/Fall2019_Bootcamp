c1e4a4f4495881c3aad412614975ce7fcfd53859
misaacs21 <m.isaacs@ufl.edu>
2019-09-18 22:06:07 -0400

    completed project (no config just so u kno)

--- queries.js
+++ queries.js
@@ -1,39 +1,45 @@
-/* Add all the required libraries*/
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
+    config = require('./config');
 
-/* Connect to your database using mongoose - remember to keep your key secret*/
-
-/* Fill out these functions using Mongoose queries*/
-//Check out - https://mongoosejs.com/docs/queries.html
+mongoose.connect(config.db.uri);
+var getModule = mongoose.model('Listing',Listing.ListingSchema);
 
 var findLibraryWest = function() {
-  /* 
-    Find the document that contains data corresponding to Library West,
-    then log it to the console. 
-   */
+    getModule.find({'code' : 'LBW'}, function(err, data) {
+        if (err) throw (err);
+        console.log(JSON.stringify(data));
+    });
 };
 var removeCable = function() {
-  /*
-    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
-    on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
-    and remove this listing from your database and log the document to the console. 
-   */
+    getModule.find({'code' : 'CABL'}, function(err, data) {
+        if (err) throw (err);
+        console.log(JSON.stringify(data));
+        getModule.deleteOne({'code': 'CABL'}, function (err, data) {
+            if (err) throw (err);
+        });
+    });
 };
-var updatePhelpsLab = function() {
-  /*
-    Phelps Lab address is incorrect. Find the listing, update it, and then 
-    log the updated document to the console. 
-    
-    Correct Address: 1953 Museum Rd, Gainesville, FL 32603
 
-   */
+var updatePhelpsLab = function() {
+    getModule.updateOne({'code' : 'PHL'}, {'address' : '1953 Museum Rd, Gainesville, FL 32603'}, function(err,data) {
+        if (err) throw (err);
+        getModule.find({'code' : 'PHL'}, function(err,data) {
+            if (err) throw (err);
+            console.log(JSON.stringify(data));
+        });
+    });
 };
+
 var retrieveAllListings = function() {
-  /* 
-    Retrieve all listings in the database, and log them to the console. 
-   */
+    getModule.find({}, function(err,data) {
+        if (err) throw (err);
+        console.log(JSON.stringify(data));
+    });
 };
 
 findLibraryWest();
 removeCable();
-updatePhelpsMemorial();
+updatePhelpsLab();
 retrieveAllListings();
